3b59f4c3ce3383f7e6d2a505bd0847bb5c352feb
pedro-demeu <pdemeu@segware.com>
2021-12-22 11:45:48 -0300

    refactor: change imports to use module

--- src/domain/data/usecases/authentication/remote.authentication.ts
+++ src/domain/data/usecases/authentication/remote.authentication.ts
@@ -1,5 +1,5 @@
-import { AuthenticationParams } from '../../../usercases/authentication';
-import { HttpPostClient } from '../../protocols/http/http-post-client';
+import { AuthenticationParams } from '@/domain/usercases/authentication';
+import { HttpPostClient } from '@/domain/data/protocols/http/http-post-client';
 
 export class RemoteAuthentication {
   constructor(
