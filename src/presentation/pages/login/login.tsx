47d1d74758aec26b1e9a404c8a3f61e9453e4438
pedro-demeu <pdemeu@segware.com>
2022-01-05 12:02:02 -0300

    refactor: move spinner to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Styles from './login-styles.scss';
-import { Spinner } from '@/presentation/components/spinner/spinner';
 import { LoginHeader as Header } from '../../components/login-header/LoginHeader';
 import { Footer } from '@/presentation/components/footer/Footer';
 import { Input } from '@/presentation/components/input/Input';
+import { FormStatus } from '@/presentation/components/form-status/FormStatus';
 
 export const Login: React.FC = () => {
   return (
@@ -13,15 +13,10 @@ export const Login: React.FC = () => {
         <h2>Login</h2>
         <Input type="email" name="email" placeholder="Digite sua senha" />
         <Input type="password" name="password" placeholder="Digite sua senha" />
-
+        <FormStatus />
         <button className={Styles.submit} type="submit">
           Entrar
         </button>
-        <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>Erro</span>
-        </div>
       </form>
       <Footer />
     </div>
