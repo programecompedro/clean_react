29d4cbd10d29a3262e61e1c561742064c63a7b9c
pedro-demeu <pdemeu@segware.com>
2022-01-05 15:54:07 -0300

    feat: ensure login will not render spinner and error ons tart

--- src/presentation/components/form-status/FormStatus.tsx
+++ src/presentation/components/form-status/FormStatus.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Spinner } from '@/presentation/components/spinner/spinner';
 import Styles from './form-status-styles.scss';
-
+import ContextForm from '@/presentation/contexts/form/form-context';
 export const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(ContextForm);
   return (
     <>
-      <div className={Styles.errorWrap}>
-        <Spinner className={Styles.spinner} />
-        <span className={Styles.error}>Erro</span>
+      <div data-testid="error-wrap" className={Styles.errorWrap}>
+        {isLoading && <Spinner className={Styles.spinner} />}
+        {errorMessage && <span className={Styles.error}>Erro</span>}
       </div>
     </>
   );
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './login-styles.scss';
 import {
   Footer,
@@ -6,21 +6,38 @@ import {
   Input,
   LoginHeader as Header,
 } from '@/presentation/components/';
+import ContextForm from '@/presentation/contexts/form/form-context';
 
+type StateProps = {
+  isLoading: boolean;
+  errorMessage: string;
+};
 export const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: '',
+  });
+
   return (
     <div className={Styles.login}>
       <Header />
-      <form className={Styles.form}>
-        <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite sua senha" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
-        <span className={Styles.link}>Criar conta</span>
-        <FormStatus />
-        <button className={Styles.submit} type="submit">
-          Entrar
-        </button>
-      </form>
+      <ContextForm.Provider value={state}>
+        <form className={Styles.form}>
+          <h2>Login</h2>
+          <Input type="email" name="email" placeholder="Digite sua senha" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <span className={Styles.link}>Criar conta</span>
+          <FormStatus />
+          <button className={Styles.submit} type="submit">
+            Entrar
+          </button>
+        </form>
+      </ContextForm.Provider>
+
       <Footer />
     </div>
   );
